54f06fc9f0dea63d605b4694a1f83e8fb55aa17a
liang <jie18851033137@163.com>
2020-12-30 21:16:51 +0800

    测试协同开发v13.0

--- src/main/java/CategoryDAO.java
+++ src/main/java/CategoryDAO.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
 import java.util.List;
 
 public interface CategoryDAO {
     List<Category> queryAllCate();
 
     List<Category> queryCate();
+
+    HashMap<String, Category> queryById();
 }
