76d6a5c540636428fe8ef0e1c2e569918d323408
liang <jie18851033137@163.com>
2020-12-30 19:47:33 +0800

    å合并了分支的方法 v6.0

---
+++ src/main/java/LiangTestGit.java
@@ -0,0 +1,9 @@
+public class LiangTestGit {
+    String first = "添加一个测试类版本4.0";
+
+    public static void test() {
+        String dev = "dev分支的一个函数";
+    }
+
+}
+
--- src/main/java/UserServiceTest.java
+++ src/main/java/UserServiceTest.java
@@ -2,6 +2,5 @@ public interface UserServiceTest {
     //这是第一次创建的方法
     void FirstMethod();
 
-    //添加一个变量a
-    String a = null;
+    String a = "添加一个变量后";
 }
