0c39db4088e73b3b624874b1b459f13ff1550807
liang <jie18851033137@163.com>
2020-12-30 19:58:58 +0800

    测试提交到github v8.0

--- src/main/java/LiangTestGit.java
+++ src/main/java/LiangTestGit.java
@@ -1,12 +1,17 @@
 public class LiangTestGit {
     String first = "添加一个测试类版本4.0";
 
-    public static void test() {
+    public static void test1() {
         String dev = "dev分支的一个函数";
         System.out.println("=========");
         System.out.println("本地修改，上传到github");
         System.out.println("=========");
     }
 
+    public static void test2() {
+        System.out.println("=========");
+        System.out.println("本地修改，上传到github");
+        System.out.println("=========");
+    }
 }
 
